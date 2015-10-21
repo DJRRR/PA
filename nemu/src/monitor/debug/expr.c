0f000ef15c42380c24305fa5c389755d33363d3f
tracer-ics2015 <tracer@njuics.org>
2015-10-21 21:41:12 +0800

    >  compile NEMU 14307130122 djr Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  21:41:12 up  2:49,  2 users,  load average: 0.17, 0.06, 0.06 cb9fbc8aa5f642f42b42ad1ec6fa85d716aa06e1

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -107,7 +107,7 @@ static bool make_token(char *e) {//shibie token
 						tokens[i].type='d';
 						int j,k=0;
 						for(j=position;j<substr_len;j++){
-							tokens[i].str[k++]=substr_start[j];
+							tokens[i].str[k++]=e[j];
 						}
 					   break;
 					case EQ:
