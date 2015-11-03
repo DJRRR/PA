c6510279430b2f0245f8b78f5ed9b4b959537791
tracer-ics2015 <tracer@njuics.org>
2015-11-03 22:28:59 +0800

    >  compile NEMU 14307130122 djr Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  22:28:59 up 42 min,  2 users,  load average: 0.00, 0.01, 0.05 52a94209aaeb4818d17db6f0cfb0f7b9b356a40

--- nemu/src/monitor/debug/watchpoint.c
+++ nemu/src/monitor/debug/watchpoint.c
@@ -20,7 +20,7 @@ void init_wp_list() {
 /* TODO: Implement the functionality of watchpoint */
 
 bool new_wp(char *e){
-/*	bool success=true;
+	bool success=true;
 	uint32_t ans;
 	WP* find=free_;
 	ans=expr(e,&success);
@@ -52,31 +52,26 @@ bool new_wp(char *e){
 	}
 	search->next=find;
 	find->next=NULL;
-	return true;*/
-//	printf("sss\n");
-	bool success=true;
+	return true;
+/*	bool success=true;
 	uint32_t temp;
 	temp=expr(e,&success);
-//	printf("ddd\n");
 	if(!success){
 		printf("EXPR ERROE at watchpoint!\n");
 		return false;
 	}
-//	printf("ddd\n");
 	if(free_==NULL){
 		printf("There is no free watchpoints!\n");
 		return false;
 	}
-//	printf("%d\n",free_->NO);
 	WP *find=free_;
 	free_=free_->next;
 	find->expr=e;
-//	printf("test2\n");
 	find->ans=temp;
 	find->next=head;
 	head=find;
 	printf("Watchpoint %d at %s\n",find->NO,find->expr);
-	return true;
+	return true;*/
 
 
 }
