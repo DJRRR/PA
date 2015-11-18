863278595355558808993ddb394968a0d6d557da
tracer-ics2015 <tracer@njuics.org>
2015-11-18 13:30:31 +0800

    >  compile NEMU 14307130122 djr Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  13:30:31 up  1:35,  2 users,  load average: 0.10, 0.04, 0.05 6c0815e5f45f65e5ff840c3df53573f815e4dfad

--- nemu/src/cpu/exec/arith/sub-template.h
+++ nemu/src/cpu/exec/arith/sub-template.h
@@ -3,7 +3,7 @@
 #define instr sub
 
 static void do_execute(){
-	printf("here\n");
+//	printf("here\n");
 	DATA_TYPE result = op_dest->val - op_src->val;
 	OPERAND_W(op_dest,result);
 	/*UPDATE EFLAGS*/
