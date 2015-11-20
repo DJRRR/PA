d0aaa3ec7763b619225e67cb476837c6b8668f16
tracer-ics2015 <tracer@njuics.org>
2015-11-20 11:35:10 +0800

    >  compile NEMU 14307130122 djr Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  11:35:10 up  1:44,  2 users,  load average: 0.08, 0.03, 0.05 cebdd37dfdbc8ef9562a7a69a510fb63fb88af2

--- nemu/src/cpu/exec/logic/call-template.h
+++ nemu/src/cpu/exec/logic/call-template.h
@@ -3,16 +3,9 @@
 #define instr call
 
 static void do_execute(){
-	cpu.esp -= 4;
-	op_dest->type=OP_TYPE_MEM;
-    op_dest->addr=cpu.esp;
-//	MEM_W(cpu.esp,cpu.eip);
-//	cpu.eip += op_src->val;
-	OPERAND_W(op_dest,cpu.eip+5);
-	cpu.eip += (DATA_TYPE_S) op_src->val;
-    op_src->val=cpu.eip;
-	printf("%0X\n",cpu.eip+5);
-    print_asm_template1();
+	MEM_W(cpu.esp,cpu.eip);
+	cpu.eip += op_src->val;
+	print_asm_template1();
 }
 
 make_instr_helper(i)
