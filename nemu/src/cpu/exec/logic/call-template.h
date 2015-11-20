418e985f75e6091c19720f598ea28211bdcfa3ac
tracer-ics2015 <tracer@njuics.org>
2015-11-20 11:15:48 +0800

    >  compile NEMU 14307130122 djr Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  11:15:48 up  1:25,  2 users,  load average: 0.00, 0.01, 0.05 8fa0464f2c4268df189747914a3981b09691d004

--- nemu/src/cpu/exec/logic/call-template.h
+++ nemu/src/cpu/exec/logic/call-template.h
@@ -5,7 +5,7 @@
 static void do_execute(){
 	cpu.esp -= 4;
 	MEM_W(cpu.esp,cpu.eip);
-	cpu.eip += op_src->val;
+//	cpu.eip += op_src->val;
 	printf("TEST: %u\n",cpu.eip);
     op_src->val=cpu.eip;
     print_asm_template1();
