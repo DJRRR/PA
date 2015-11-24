ecfe8a803f4054858818831f46a90f5afb97684a
tracer-ics2015 <tracer@njuics.org>
2015-11-24 14:50:12 +0800

    >  compile NEMU 14307130122 djr Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  14:50:12 up  5:54,  3 users,  load average: 0.07, 0.03, 0.05 c120cc75bda0f1fb8e6ac975b1250d50b9c69e69

--- nemu/src/cpu/exec/logic/call-template.h
+++ nemu/src/cpu/exec/logic/call-template.h
@@ -3,15 +3,41 @@
 #define instr call
 
 static void do_execute(){
-	cpu.esp -= 4;
+/*	cpu.esp -= 4;
 	MEM_W(cpu.esp,cpu.eip+1+DATA_BYTE);
 	cpu.eip += op_src->val;
 //	op_src->val = cpu.eip;
 //	printf("op_src->val : %u\n",op_src->val);
+	print_asm_template1();*/
+	if(ops_decoded.opcode==0xe8){
+		if(ops_decoded.is_data_size_16){
+			cpu.esp -= 2;
+			MEM_W(cpu.esp,(cpu.eip&0xff)+1+DATA_BYTE);
+			cpu.eip += op_src->val&0xffff;
+		}
+		else{
+			cpu.esp -= 4;
+			MEM_W(cpu.esp,cpu.eip+1+DATA_BYTE);
+		}
+	}
+	if(ops_decoded.opcode==0xff){
+		if(ops_decoded.is_data_size_16){
+			cpu.esp -= 2;
+			MEM_W(cpu.esp,(cpu.eip&0xff)+2);
+			cpu.eip=((op_src->val)&0xffff)-2;
+		}
+		else{
+			cpu.esp -= 4;
+			MEM_W(cpu.esp,cpu.eip+2);
+			cpu.eip=op_src->val-2;
+		}
+	}
 	print_asm_template1();
+
 }
 
 make_instr_helper(i)
+make_instr_helper(rm)
 
 #include "cpu/exec/template-end.h"
 
