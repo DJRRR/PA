404939832ae6ceb92beb135a7b5d21c71abfe576
tracer-ics2015 <tracer@njuics.org>
2015-11-24 13:53:28 +0800

    >  compile NEMU 14307130122 djr Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  13:53:28 up  4:57,  3 users,  load average: 0.00, 0.01, 0.05 b7808af620edb080dbd261fc61bc2d0245547a78

--- nemu/src/cpu/exec/data-mov/ret.c
+++ nemu/src/cpu/exec/data-mov/ret.c
@@ -5,14 +5,16 @@ make_helper(ret){
 	if(ops_decoded.opcode==0xc3){
 		if(ops_decoded.is_data_size_16){
 			//OPERAND_W(cpu.eip,cpu.esp&0xff);
-			cpu.eip=cpu.esp&0xff;
+		//	cpu.eip=cpu.esp&0xff;
+		    swaddr_write(cpu.eip,2,cpu.esp&0xff);
 			cpu.esp += 2;
 			cpu.eip = cpu.eip & 0xffff;
 			print_asm("ret");
 		}
 		else{
+			swaddr_write(cpu.eip,4,cpu.esp);
 		//	OPERAND_W(cpu.eip,cpu.esp);
-		    cpu.eip=cpu.esp;
+		   // cpu.eip=cpu.esp;
 			cpu.esp += 4;
 			print_asm("ret");
 		}
