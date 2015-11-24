1020a8891c93e92e0ca0f8be9d6917c9a2e77a63
tracer-ics2015 <tracer@njuics.org>
2015-11-25 00:36:02 +0800

    >  compile NEMU 14307130122 djr Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  00:36:02 up  2:35,  4 users,  load average: 0.02, 0.04, 0.05 7ec0cd34c22f671072fe2749b6e923f2b1d68fc

--- nemu/src/cpu/exec/arith/sub-template.h
+++ nemu/src/cpu/exec/arith/sub-template.h
@@ -12,33 +12,8 @@ static void do_execute(){
 	DATA_TYPE flag_res=(result>>(DATA_BYTE*8-1))&1;
 	unsigned int num=0;
 	int i=0;
-	for(i=0;i<8;i++){
-		if(result&1){
-			num++;
-		}
-		result >>= 1;
-	}
-	cpu.PF=!(num%2);
-/*	if(num%2==0){
-		cpu.PF=1;
-	}
-	else{
-		cpu.PF=0;
-	}*/
-   /* if(result==0){
-		cpu.ZF=1;
-	}
-	else{
-		cpu.ZF=0;
-	}*/
 	cpu.ZF=!result;
 	cpu.SF=flag_res;
-/*	if(flag_res==1){
-		cpu.SF=1;
-	}
-	else{
-		cpu.SF=0;
-	}*/
 	if((flag_dest==1&&flag_src==0&&flag_res==0)||(flag_dest==0&&flag_src==1&&flag_res==1)){
 		cpu.OF=1;
 	}
@@ -50,7 +25,14 @@ static void do_execute(){
 	}
 	else{
      	cpu.CF=0;
-	}	
+	}
+	for(i=0;i<8;i++){
+		if(result&1){
+			++num;
+		}
+		result >>= 1;
+	}
+cpu.PF=!(num%2);	
 
 	print_asm_template2();
 }
