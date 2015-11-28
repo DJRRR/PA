7afebd37dd6e99c454649d018896d51569dd7fbb
tracer-ics2015 <tracer@njuics.org>
2015-11-28 09:51:43 +0800

    >  compile NEMU 14307130122 djr Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  09:51:43 up 11:03,  4 users,  load average: 0.12, 0.06, 0.05 d426b90c60445303c5e0908269979ce642fdea18

--- nemu/src/monitor/debug/elf.c
+++ nemu/src/monitor/debug/elf.c
@@ -92,7 +92,7 @@ unsigned int give_num(char *s){
 	for(i=0;i<nr_symtab_entry;i++){
 	pos=symtab[i].st_name;
 	if(strcmp(s,strtab+pos)==0){
-		if((symtab[i].st_info&0x1111)==1){
+		if((symtab[i].st_info&0x11)==1){
 			return symtab[i].st_value;
 			 }
 		else {
@@ -113,12 +113,12 @@ void backtrace(unsigned int ebp){
 	while(work!=0){
 		ret=swaddr_read((work+4),4);
 		for(i=0;i<nr_symtab_entry;i++){
-	//		if((symtab[i].st_info&0x1111)==2){
+			if((symtab[i].st_info&0x11)==2){
 				if(ret>=symtab[i].st_value&&ret<=(symtab[i].st_value+symtab[i].st_size)){
 					printf("# %d: %s\n",count++,strtab+symtab[i].st_name);
 					break;
 				}
-	//		}
+			}
 		}
 	//	work=swaddr_read(work+24,4);
 	    work += 24;
