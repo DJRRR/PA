a9eb6590f6f559c3eeebc94eb7e8ed8e6c56b1ca
tracer-ics2015 <tracer@njuics.org>
2015-11-29 17:20:47 +0800

    >  compile NEMU 14307130122 djr Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  17:20:47 up  1:09,  4 users,  load average: 0.15, 0.09, 0.06 697bf427269ea9e777ba0e976540f843ce3b73bd

--- nemu/src/monitor/debug/elf.c
+++ nemu/src/monitor/debug/elf.c
@@ -109,6 +109,7 @@ void backtrace(unsigned int ebp){
 	uint32_t work=ebp;
 	int count=0;
 	int i=0;
+	int flag=0;
 	uint32_t ret=0;
 	if(work==0){
 		printf("No stack.\n");
@@ -119,6 +120,9 @@ void backtrace(unsigned int ebp){
 		ret=swaddr_read((work+4),4);
 		for(i=0;i<nr_symtab_entry;i++){
 		//	if((symtab[i].st_info&0x11)==2){
+			if(flag==0){
+				break;
+			}
 				if(ret>=symtab[i].st_value&&ret<=(symtab[i].st_value+symtab[i].st_size)){
 					printf("# %d: %s\n",count++,strtab+symtab[i].st_name);
 					break;
@@ -126,6 +130,7 @@ void backtrace(unsigned int ebp){
 		//	}
 		}
 		work=swaddr_read(work,4);
+		flag=1;
 	}
 	if(count==0){
 		printf("No stack.\n");
