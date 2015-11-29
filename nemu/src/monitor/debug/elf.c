9393c5af132e826c457c59076d88b512eb49fa6a
tracer-ics2015 <tracer@njuics.org>
2015-11-29 18:07:37 +0800

    >  compile NEMU 14307130122 djr Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  18:07:37 up  1:56,  4 users,  load average: 0.03, 0.04, 0.05 fb3f28e3a6aa9142a6c65bfe5ffac2bc1e5d4d

--- nemu/src/monitor/debug/elf.c
+++ nemu/src/monitor/debug/elf.c
@@ -127,9 +127,6 @@ void backtrace(unsigned int ebp,unsigned int eip){
 	}
 	while(work!=0){
 		ret=swaddr_read(work+4,4);
-        if(swaddr_read(work,4)==0){
-			break;
-		}
 		for(i=0;i<nr_symtab_entry;i++){
 		//	if((symtab[i].st_info&0x11)==2){
 				if(ret>=symtab[i].st_value&&ret<=(symtab[i].st_value+symtab[i].st_size)){
