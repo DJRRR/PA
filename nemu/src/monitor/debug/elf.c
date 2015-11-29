9b8213e2e229ab020c42afdb22b41fe4bb1873df
tracer-ics2015 <tracer@njuics.org>
2015-11-29 17:37:32 +0800

    >  compile NEMU 14307130122 djr Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  17:37:32 up  1:26,  4 users,  load average: 0.05, 0.04, 0.05 e5af413ff0b299af3b97cc282091845d81929fe4

--- nemu/src/monitor/debug/elf.c
+++ nemu/src/monitor/debug/elf.c
@@ -120,6 +120,9 @@ void backtrace(unsigned int ebp){
 		printf("ADDR:%u\n",ret);
 		for(i=0;i<nr_symtab_entry;i++){
 		//	if((symtab[i].st_info&0x11)==2){
+				if(swaddr_read(work,4)==0){
+					break;
+				}
 				if(ret>=symtab[i].st_value&&ret<=(symtab[i].st_value+symtab[i].st_size)){
 					printf("# %d: %s\n",count++,strtab+symtab[i].st_name);
 					break;
