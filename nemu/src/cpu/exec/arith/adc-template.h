60a3115bd32848b6209c6d524ba27defb1859466
tracer-ics2015 <tracer@njuics.org>
2016-02-25 20:36:20 +0800

    >  compile NEMU 14307130122 djr Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  20:36:20 up 23 min,  4 users,  load average: 0.00, 0.01, 0.02 dd65fd8ff9e704bd72ec88034e9c87c18889cdbb

--- nemu/src/cpu/exec/arith/adc-template.h
+++ nemu/src/cpu/exec/arith/adc-template.h
@@ -6,15 +6,67 @@ static void do_execute(){
 	DATA_TYPE result_1=op_dest->val+op_src->val;
 	DATA_TYPE tmp1=op_dest->val;
 	DATA_TYPE temp1=(~op_src->val+1);
-//	unsigned int num=0;
-//	int i=0;
+	unsigned int num=0;
+	int i=0;
 	if(tmp1<temp1){
 		cpu.CF=1;
 		DATA_TYPE result_2=result_1+cpu.CF;
+		DATA_TYPE tmp2=result_1;
+		DATA_TYPE temp2=(~cpu.CF+1);
+		DATA_TYPE flag_dest_1=MSB(result_1)&1;
+		DATA_TYPE flag_src_1=MSB(cpu.CF)&1;
+		DATA_TYPE flag_res_1=MSB(result_2)&1;
 		OPERAND_W(op_dest,result_2);
+		if(tmp2<temp2){
+			cpu.CF=1;
+		}
+		else{
+			cpu.CF=0;
+		}
+		cpu.ZF=!result_2;
+		cpu.SF=flag_res_1;
+		if((flag_dest_1==1&&flag_src_1==1&&flag_res_1==0)||(flag_dest_1==0&&flag_src_1==0&&flag_res_1==1)){
+			cpu.OF=1;
+		}
+		else{
+			cpu.OF=0;
+		}
+		for(i=0;i<8;i++){
+			if(result_2&1){
+				num++;
+			}
+			result_2 >>= 1;
+		}
+		cpu.PF=!(num%2);
 	}
-		
-		
+
+	else{
+		DATA_TYPE flag_dest_2=MSB(op_dest->val)&1;
+		DATA_TYPE flag_src_2=MSB(op_src->val)&1;
+		DATA_TYPE flag_res_2=MSB(result_1)&1;
+		OPERAND_W(op_dest,result_1);
+		if(tmp1<temp1){
+			cpu.CF=1;
+		}
+		else{
+			cpu.CF=0;
+		}
+		cpu.ZF=!result_1;
+		cpu.SF=flag_res_2;
+		if((flag_dest_2==1&&flag_src_2==1&&flag_res_2==0)||(flag_dest_2==0&&flag_src_2==0&&flag_res_2==1)){
+			cpu.OF=1;
+		}
+		else{
+			cpu.OF=0;
+		}
+		for(i=0;i<8;i++){
+			if(result_1&1){
+				num++;
+			}
+			result_1 >>= 1;
+		}
+		cpu.PF=!(num%2);
+	}	
 	print_asm_template2();
 
 }
