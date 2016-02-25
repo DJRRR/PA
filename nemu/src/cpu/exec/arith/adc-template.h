ec07622623cb3d059c36784da570d93f73b553e8
tracer-ics2015 <tracer@njuics.org>
2016-02-25 20:24:20 +0800

    >  compile NEMU 14307130122 djr Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  20:24:20 up 11 min,  4 users,  load average: 0.08, 0.03, 0.02 27b5d196505fb45f178a345ce7b4ecbf3295879b

--- nemu/src/cpu/exec/arith/adc-template.h
+++ nemu/src/cpu/exec/arith/adc-template.h
@@ -3,46 +3,18 @@
 #define instr adc
 
 static void do_execute(){
-	DATA_TYPE result = op_dest->val + op_src->val + cpu.CF;
+	DATA_TYPE result_1=op_dest->val+op_src->val;
 	DATA_TYPE tmp1=op_dest->val;
-	DATA_TYPE tmp2=op_dest->val+cpu.CF;
-	OPERAND_W(op_dest,result);
-	DATA_TYPE temp1=(~cpu.CF+1);
-	DATA_TYPE temp2=(~op_src->val+1);
-	DATA_TYPE flag_dest=MSB(op_dest->val)&1;
-	DATA_TYPE flag_src=MSB(op_src->val)&1;
-	DATA_TYPE flag_res=MSB(result)&1;
-	unsigned int num=0;
-	int i=0;
+	DATA_TYPE temp1=(~op_src->val+1);
+//	unsigned int num=0;
+//	int i=0;
 	if(tmp1<temp1){
 		cpu.CF=1;
+		DATA_TYPE result_2=result_1+cpu.CF;
+		OPERAND_W(op_dest,result_2);
 	}
-	else{
-		if(tmp2<temp2){
-			cpu.CF=1;
-		}
-		else{
-			cpu.CF=0;
-		}
-	}
-	cpu.ZF=!result;
-	cpu.SF=flag_res;
-	if(cpu.CF==0){
-		if((flag_dest==1&&flag_src==1&&flag_res==0)||(flag_dest==0&&flag_src==0&&flag_res==1)){
-			cpu.OF=1;
-		}
-		else{
-			cpu.OF=0;
-		}
-	}
-	/*uncompleted*/
-	for(i=0;i<8;i++){
-		if(result&1){
-			num++;
-		}
-		result >>= 1;
-	}
-	cpu.PF=!(num%2);
+		
+		
 	print_asm_template2();
 
 }
