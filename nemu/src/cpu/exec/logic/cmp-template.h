fc4c50f9f9cc1993f4afc276dfde0ef80201ca6f
tracer-ics2015 <tracer@njuics.org>
2016-02-26 11:01:22 +0800

    >  compile NEMU 14307130122 djr Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  11:01:22 up  1:02,  3 users,  load average: 0.05, 0.03, 0.02 c5f0c3eb57af66dfa4f552f3663326d37728dc2d

--- nemu/src/cpu/exec/logic/cmp-template.h
+++ nemu/src/cpu/exec/logic/cmp-template.h
@@ -4,17 +4,20 @@
 
 static void do_execute(){
 	DATA_TYPE result= op_dest->val-(DATA_TYPE_S)op_src->val;
+	DATA_TYPE flag_dest=MSB(op_dest->val)&1;
+	DATA_TYPE flag_src=MSB(op_src->val)&1;
+	DATA_TYPE flag_res=MSB(result)&1;
 	unsigned int num=0;
 	int i=0;
 	cpu.ZF=!result;
 	cpu.SF=MSB(result);
-	if((MSB(op_dest->val)==1&&MSB(op_src->val)==0&&MSB(result)==0)||(MSB(op_dest->val)==0&&MSB(op_src->val)==1&&MSB(result)==1)){
+	if((flag_dest==1&&flag_src==0&&flag_res==0)||(flag_dest==0&&flag_src&&flag_res==1)){
 		cpu.CF=1;
 	}
 	else{
 		cpu.CF=0;
 	}
-	if(op_dest->val<op_src->val){
+	if(op_dest->val<(DATA_TYPE_S)op_src->val){
 		cpu.CF=1;
 	}
 	else{
