23bf18df452508651ab16b99aa1e588393a5cd42
tracer-ics2015 <tracer@njuics.org>
2016-02-26 18:21:59 +0800

    >  compile NEMU 14307130122 djr Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  18:21:59 up  3:07,  4 users,  load average: 0.00, 0.01, 0.05 a58b6b34575e180cf8f9f5d9c33d7242e2b76c7b

--- nemu/src/cpu/exec/exec.c
+++ nemu/src/cpu/exec/exec.c
@@ -65,12 +65,12 @@ make_group(group2_cl_v,
 /* 0xf6 */
 make_group(group3_b,
 	test_i2rm_b, inv, not_rm_b, inv, 
-	inv, imul_rm2a_b, inv, inv)
+	mul_rm_b, imul_rm2a_b, inv, inv)
 
 /* 0xf7 */
 make_group(group3_v,
 	test_i2rm_v, inv, not_rm_v, inv, 
-	inv, imul_rm2a_v, inv, inv)
+	mul_rm_v, imul_rm2a_v, inv, inv)
 
 /* 0xfe */
 make_group(group4,
