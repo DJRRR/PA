6417e01a13563c2bf8978fdb08d049e1afaa643a
tracer-ics2015 <tracer@njuics.org>
2016-02-28 20:24:35 +0800

    >  compile NEMU 14307130122 djr Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  20:24:35 up 10:24,  4 users,  load average: 0.00, 0.01, 0.05 c7fec49e5e57f498b55476da45bd029f6754a1a3

--- nemu/src/cpu/exec/arith/inc-template.h
+++ nemu/src/cpu/exec/arith/inc-template.h
@@ -7,8 +7,33 @@ static void do_execute () {
 	OPERAND_W(op_src, result);
 
 	/* TODO: Update EFLAGS. */
-	panic("please implement me");
-
+	DATA_TYPE src2=1;
+	DATA_TYPE temp=(~src2+1);
+	DATA_TYPE flag_src=MSB(op_src->val)&1;
+	DATA_TYPE flag_res=MSB(result)&1;
+	cpu.ZF=!result;
+	cpu.SF=flag_res;
+	unsigned int num=0;
+	int i=0;
+	if(flag_src==0&&flag_res==1){
+		cpu.OF=1;
+	}
+	else{
+		cpu.OF=0;
+	}
+	if(op_src->val<temp){
+		cpu.CF=1;
+	}
+	else{
+		cpu.CF=0;
+	}
+	for(i=0;i<8;i++){
+		if(result&1){
+			num++;
+		}
+		result >>= 1;
+	}
+	cpu.PF=!(num%2);
 	print_asm_template1();
 }
 
--- nemu/src/cpu/exec/exec.c
+++ nemu/src/cpu/exec/exec.c
@@ -74,13 +74,13 @@ make_group(group3_v,
 
 /* 0xfe */
 make_group(group4,
-	inv, inv, inv, inv, 
+	inc_rm_b, inv, inv, inv, 
 	inv, inv, inv, inv)
 
 /* 0xff */
 make_group(group5,
-	inv, inv, call_rm_v, inv, 
-	jmp_rm_v, inv, push_rm_v, inv)
+	inc_rm_v, inv, call_rm_v, inv, 
+	jmp_rm_v, inv, inc_rm_v, inv)
 
 make_group(group6,
 	inv, inv, inv, inv, 
@@ -110,7 +110,7 @@ helper_fun opcode_table [256] = {
 /* 0x34 */	xor_i2a_b, xor_i2a_v, inv, inv,
 /* 0x38 */	cmp_r2rm_b, cmp_r2rm_v, cmp_rm2r_b, cmp_rm2r_v,
 /* 0x3c */	cmp_i2a_b, cmp_i2a_v, inv, inv,
-/* 0x40 */	inv, inv, inv, inv,
+/* 0x40 */	inc_r_v, inv, inv, inv,
 /* 0x44 */	inv, inv, inv, inv,
 /* 0x48 */	inv, inv, inv, inv,
 /* 0x4c */	inv, inv, inv, inv,
