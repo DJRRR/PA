f5f8cdbae486cb5f78becf16a2fe87122bf954b6
tracer-ics2015 <tracer@njuics.org>
2016-02-29 22:03:59 +0800

    >  compile NEMU 14307130122 djr Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  22:03:59 up  5:39,  4 users,  load average: 0.00, 0.01, 0.05 11c72222f5c3a784e68a1265756e560792f28eab

--- nemu/src/cpu/exec/data-mov/jmp-template.h
+++ nemu/src/cpu/exec/data-mov/jmp-template.h
@@ -3,7 +3,7 @@
 #define instr jmp
 
 static void do_execute(){
-/*	if(ops_decoded.opcode==0xff){
+	if(ops_decoded.opcode==0xff){
 			cpu.eip=op_src->val;
 			if(DATA_BYTE==2){
 				cpu.eip = cpu.eip&0x0000ffff;
@@ -21,8 +21,8 @@ static void do_execute(){
 		if(ops_decoded.is_data_size_16){
 			cpu.eip=cpu.eip&0x0000ffff;
 		}
-	}*/
-	swaddr_t opcode=instr_fetch(cpu.eip,1);
+	}
+/*	swaddr_t opcode=instr_fetch(cpu.eip,1);
 	DATA_TYPE_S addr=instr_fetch(cpu.eip+1,DATA_BYTE);
 	if(opcode==0xeb||opcode==0xe9){
 		cpu.eip += addr;
@@ -38,7 +38,7 @@ static void do_execute(){
 		else{
 			cpu.eip -= 7;
 		}
-	}
+	}*/
 	print_asm_template1();
 }
 
