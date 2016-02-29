4c0033cdd73995743eeaf79fb2e539539546d2e5
tracer-ics2015 <tracer@njuics.org>
2016-02-29 21:39:38 +0800

    >  compile NEMU 14307130122 djr Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  21:39:38 up  5:15,  4 users,  load average: 0.09, 0.11, 0.12 a258547a3d5da51258e451c4d2b03133d1befd5

--- nemu/src/cpu/exec/data-mov/leave.c
+++ nemu/src/cpu/exec/data-mov/leave.c
@@ -1,17 +1,18 @@
 #include "cpu/exec/helper.h"
 #include "cpu/decode/modrm.h"
 make_helper(leave){
-	cpu.esp=cpu.ebp;
+
 	if(ops_decoded.is_data_size_16){
+		cpu.esp=cpu.gpr[5]._16;
 		cpu.gpr[5]._16=swaddr_read(cpu.esp,2);
 		cpu.esp += 2;
 		print_asm("leave");
 	}
 	else{
+		cpu.esp=cpu.ebp;
 		cpu.ebp=swaddr_read(cpu.esp,4);
 		cpu.esp +=4;
 		print_asm("leave");
 	}
 	return 1;
 }
-
