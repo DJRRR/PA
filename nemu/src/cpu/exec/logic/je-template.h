91ad9498b2b55a03fb476aa12839949283cafaab
tracer-ics2015 <tracer@njuics.org>
2016-03-01 12:17:05 +0800

    >  compile NEMU 14307130122 djr Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  12:17:05 up  3:34,  3 users,  load average: 0.00, 0.01, 0.05 c90b570a5a0baf135486155a4872776e22fe3c8

--- nemu/src/cpu/exec/logic/je-template.h
+++ nemu/src/cpu/exec/logic/je-template.h
@@ -4,7 +4,7 @@
 
 static void do_execute(){
 	if(cpu.ZF==1){
-		cpu.eip += (instr_fetch(cpu.eip+1,DATA_BYTE)<<(32-DATA_BYTE*8))>>(32-DATA_BYTE*8);
+		cpu.eip += (DATA_TYPE_S)op_src->val;
 		if(ops_decoded.is_data_size_16){
 			cpu.eip = cpu.eip&0x0000ffff;
 		}
