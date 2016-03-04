b25fead209b0ace5e7ccaf63caa485dd852c6021
tracer-ics2015 <tracer@njuics.org>
2016-03-04 14:33:30 +0800

    >  compile kernel 14307130122 djr Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  14:33:30 up  6:17,  3 users,  load average: 0.01, 0.02, 0.05 e7499202ae5cde601ca09245508ae1531c13a8db

--- kernel/src/elf/elf.c
+++ kernel/src/elf/elf.c
@@ -51,7 +51,7 @@ uint32_t loader() {
 			/* TODO: zero the memory region 
 			 * [VirtAddr + FileSiz, VirtAddr + MemSiz)
 			 */
-			memset(va_to_pa(ph->p_vaddr+ph->p_filesz),0,ph->p_memsz-ph->p_filesz);
+			memset(va_to_pa(ph->p_vaddr+ph->p_filesz+ph->p_offset),0,ph->p_memsz-ph->p_filesz);
 
 #ifdef IA32_PAGE
 			/* Record the program break for future use. */
