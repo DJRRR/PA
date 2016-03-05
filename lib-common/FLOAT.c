52ed1bcbc5c65613de7af6496477bb316ea71853
tracer-ics2015 <tracer@njuics.org>
2016-03-05 11:19:49 +0800

    >  run 14307130122 djr Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  11:19:49 up  1:22,  4 users,  load average: 0.02, 0.13, 0.13 32c309934428dc5d32f0eff5bb3a298d3ae2ea28

--- lib-common/FLOAT.c
+++ lib-common/FLOAT.c
@@ -1,17 +1,15 @@
 #include "FLOAT.h"
 
 FLOAT F_mul_F(FLOAT a, FLOAT b) {
-	long long  a_t=a&0x7fffffff;
-	long long  b_t=b&0x7fffffff;
-	int flag_a=a&0x80000000;
-	int flag_b=b&0x80000000;
+	long long  a_t=a;
+	long long  b_t=b;
 	long long  result=a_t*b_t;
 	FLOAT fin;
-	if(flag_a==flag_b){
+	if(result>=0){
 		fin=result>>16;
 	}
 	else{
-		fin=(-result>>16);
+		fin=-(-result>>16);
 	}
 	return fin;
 /*	long long tmp=(long long)a*(long long)b;
