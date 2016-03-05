76e943993e66256a40748aa58751ab8acbcdde53
tracer-ics2015 <tracer@njuics.org>
2016-03-05 11:11:44 +0800

    >  run 14307130122 djr Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  11:11:44 up  1:14,  4 users,  load average: 0.00, 0.05, 0.10 e34c48d4638717fe70b860d5c1deccf74a53b7f

--- lib-common/FLOAT.c
+++ lib-common/FLOAT.c
@@ -28,7 +28,7 @@ FLOAT F_div_F(FLOAT a, FLOAT b) {
 }
 
 FLOAT f2F(float a) {
-	float temp=a;
+/*	float temp=a;
 	FLOAT fin;
 	int temp_i=*((int *)&temp);
 	int flag=(temp_i>>31)&1;
@@ -45,7 +45,25 @@ FLOAT f2F(float a) {
 	else{
 		fin=(F_val<<off_judge)|(flag<<31);
 	}
-	return fin;
+	return fin;*/
+	float temp=a;
+//	FLOAT fin;
+	int temp_i=*((int *)&temp);
+	int flag=(temp_i>>31)&1;
+	int exp=((temp_i>>23)&0xff)-127;
+	int f2=(temp_i&0x7fff)|0x800000;
+	int offset=23-exp;
+	int t=((f2>>offset)<<16)&0x7fff0000;
+	f2=((f2<<(32-offset)))&0x0000ffff;
+	int F=f2+t;
+	if(flag==1){
+	return F;
+	}
+	else{
+	return -F;
+	}
+
+
 }
 
 FLOAT Fabs(FLOAT a) {
