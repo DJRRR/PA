2b3c839b506ffa360a1226a180ddc97f4655004b
tracer-ics2015 <tracer@njuics.org>
2016-03-06 16:01:13 +0800

    >  compile NEMU 14307130122 djr Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  16:01:13 up  4:39,  4 users,  load average: 0.00, 0.01, 0.05 1f264317187c41e157cc0bd876bd7123f7c90a12

--- nemu/src/memory/cache.c
+++ nemu/src/memory/cache.c
@@ -2,11 +2,17 @@
 #include<time.h>
 #include <stdlib.h>
 
+uint32_t dram_read(hwaddr_t,size_t);
+uint32_t hwaddr_read(hwaddr_t,size_t);
+void dram_write(hwaddr_t,size_t,uint32_t);
+void hwaddr_write(hwaddr_t,size_t,uint32_t);
+
 typedef struct{
 	unsigned int valid:1;
 	unsigned int tag:14;
 	unsigned int index:7;
 	unsigned int offset:6;
+	uint8_t data[64];
 }Cache_L1;
 
 Cache_L1 cache_L1[8][128];//8-way
@@ -34,5 +40,60 @@ bool find_cache_L1(hwaddr_t addr,size_t len){
 	}
 	return false;
 }
+
+uint32_t read_cache_L1(hwaddr_t addr,size_t len){
+	unsigned int offset_i=addr&0x3f;
+	unsigned int index_i=(addr&0x1fc0)>>6;
+	unsigned int tag_i=(addr&0xfffe000)>>13;
+	bool flag=find_cache_L1(addr,len);
+	int i,way_i=-1;
+	bool check=false;
+	if(flag==true){
+		printf("here1\n");
+		for(i=0;i<8;i++){
+			if(cache_L1[i][index_i].tag==tag_i){
+				way_i=i;
+				check=true;
+				break;
+			}
+		}
+		if(check==false){
+		//	printf("Read cache L1 error!\n");
+		assert(0);
+		}
+		uint32_t res=0x00000000;
+		if(offset_i+len<=64){//check bound
+			int j=0;
+			for(j=0;j<len;j++){
+				res = (res<<8)|cache_L1[way_i][index_i].data[offset_i+j];//unchecked
+			}
+			return res;
+		}
+		else{
+			printf("cache 1 bound error!\n");
+			assert(0);
+		}
+	}
+	else{
+		int num;
+		srand((unsigned)time(NULL));
+		num=rand()%128;
+		int i_i=(tag_i)%8;
+		cache_L1[i_i][num].valid=1;
+		cache_L1[i_i][num].tag=tag_i;
+		cache_L1[i_i][num].index=num;
+		cache_L1[i_i][num].data[0]=dram_read(addr&0xfffffff8,8);
+		return dram_read(addr,len);
+	}
+}
+
+
+
+
+
+
+			
+
+
 
 
