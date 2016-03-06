02f5130ee6258dca8cf430c8e250c562a7bfa838
tracer-ics2015 <tracer@njuics.org>
2016-03-06 14:42:57 +0800

    >  compile NEMU 14307130122 djr Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  14:42:57 up  3:20,  4 users,  load average: 0.00, 0.01, 0.05 4af44e3a4b8ea1644e8303216be57f324cbce16f

--- nemu/src/memory/cache.c
+++ nemu/src/memory/cache.c
@@ -1,1 +1,38 @@
-#
+#include <common.h>
+#include<time.h>
+#include <stdlib.h>
+
+typedef struct{
+	unsigned int valid:1;
+	unsigned int tag:14;
+	unsigned int index:7;
+	unsigned int offset:6;
+}Cache_L1;
+
+Cache_L1 cache_L1[8][128];//8-way
+
+void init_cache_L1(){
+	int i,j;
+	for(i=0;i<8;i++){
+		for(j=0;j<128;j++){
+			cache_L1[i][j].valid=0;
+		}
+	}
+}
+
+bool find_cache_L1(hwaddr_t addr,size_t len){
+//	unsigned int offset_i=addr&0x3f;
+	unsigned int index_i=(addr&0x1fc0)>>6;
+	unsigned int tag_i=(addr&0xfffe000)>>13;
+	int i=0;
+	for(;i<8;i++){
+		if(tag_i==cache_L1[i][index_i].tag){
+			if(cache_L1[i][index_i].valid==1){
+				return true;
+			}
+		}
+	}
+	return false;
+}
+
+
