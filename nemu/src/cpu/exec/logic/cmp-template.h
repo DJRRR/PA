b1195d346bbda218f98741ff01fd1667fc8a48e9
tracer-ics2015 <tracer@njuics.org>
2016-03-10 16:14:54 +0800

    >  compile NEMU 14307130122 djr Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  16:14:54 up 36 min,  3 users,  load average: 0.46, 0.42, 0.36 530d49f926d9500df502db45685406fba750ac40

--- nemu/src/cpu/exec/logic/cmp-template.h
+++ nemu/src/cpu/exec/logic/cmp-template.h
@@ -7,8 +7,8 @@ static void do_execute(){
 	DATA_TYPE flag_dest=MSB(op_dest->val)&1;
 	DATA_TYPE flag_src=MSB(op_src->val)&1;
 	DATA_TYPE flag_res=MSB(result)&1;
-	unsigned int num=0;
-	int i=0;
+//	unsigned int num=0;
+//	int i=0;
 	cpu.ZF=!result;
 	cpu.SF=flag_res;
 	if((flag_dest==1&&flag_src==0&&flag_res==0)||(flag_dest==0&&flag_src==1&&flag_res==1)){
@@ -23,13 +23,19 @@ static void do_execute(){
 	else{
 		cpu.CF=0;
 	}
-	for(i=0;i<8;i++){
+/*	for(i=0;i<8;i++){
 		if(result&1){
 			++num;
 		}
 		result >>= 1;
 	}	
-	cpu.PF=!(num%2);
+	cpu.PF=!(num%2);*/
+	DATA_TYPE res_t=result;
+	res_t = res_t &0xff;
+	res_t ^= res_t>>4;
+	res_t ^= res_t >>2;
+	res_t ^= res_t>>1;
+	cpu.PF=!res_t;
 
 	print_asm_template2();
 
