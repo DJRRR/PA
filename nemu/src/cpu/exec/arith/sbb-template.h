75e4773d30c3a4f9138b28504517a7b25ef09bbb
tracer-ics2015 <tracer@njuics.org>
2016-03-10 16:04:41 +0800

    >  compile NEMU 14307130122 djr Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  16:04:41 up 25 min,  3 users,  load average: 0.15, 0.43, 0.35 be9167c918cd0950d9a573f9eef9823868cf695d

--- nemu/src/cpu/exec/arith/sbb-template.h
+++ nemu/src/cpu/exec/arith/sbb-template.h
@@ -9,21 +9,27 @@ static void do_execute(){
 	}
 	DATA_TYPE result=op_dest->val-(src+cpu.CF);
 	OPERAND_W(op_dest,result);
-	DATA_TYPE temp=result;
+//	DATA_TYPE temp=result;
 	DATA_TYPE flag_dest=MSB(op_dest->val)&1;
 	DATA_TYPE flag_src=MSB(src)&1;
 	DATA_TYPE flag_res=MSB(result)&1;
-	unsigned int num=0;
-	int i=0;
+//	unsigned int num=0;
+//	int i=0;
 	cpu.SF=flag_res;
 	cpu.ZF=!result;
-	for(i=0;i<8;i++){
+/*	for(i=0;i<8;i++){
 		if(temp&1){
 			num++;
 		}
 		temp >>= 1;
 	}
-	cpu.PF=!(num%2);
+	cpu.PF=!(num%2);*/
+	DATA_TYPE res_t=result;
+	res_t = res_t &0xff;
+	res_t ^= res_t>>4;
+	res_t ^= res_t>>2;
+	res_t ^= res_t>>1;
+	cpu.PF=!res_t;
 	if(op_dest->val<src){
 		cpu.CF=1;
 	}
