24327d670ed428063cd21d84d3a15095c5b3034f
tracer-ics2015 <tracer@njuics.org>
2016-03-10 16:17:53 +0800

    >  compile NEMU 14307130122 djr Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  16:17:53 up 39 min,  3 users,  load average: 0.20, 0.38, 0.37 c533828887cec45ff8973a9ef3aaac16edf82430

--- nemu/src/cpu/exec/logic/test-template.h
+++ nemu/src/cpu/exec/logic/test-template.h
@@ -7,21 +7,23 @@ static void do_execute(){
 	cpu.OF=0;
 	cpu.CF=0;
 	unsigned int flag_res=MSB(result)&1;
-	unsigned int num=0;
-	int i=0;
-	if(result==0){
+//	unsigned int num=0;
+//	int i=0;
+/*	if(result==0){
 		cpu.ZF=1;
 	}
 	else{
 		cpu.ZF=0;
-	}
-	if(flag_res==1){
+	}*/
+	cpu.ZF=!result;
+/*	if(flag_res==1){
 		cpu.SF=1;
 	}
 	else{
 		cpu.SF=0;
-	}
-	for(i=0;i<8;i++){
+	}*/
+	cpu.SF=flag_res;
+/*	for(i=0;i<8;i++){
 		if((result&1)==1){
 			num++;
 		}
@@ -32,7 +34,13 @@ static void do_execute(){
 	}
 	else{
 		cpu.PF=0;
-	}
+	}*/
+	DATA_TYPE res_t=result;
+	res_t = res_t &0xff;
+	res_t ^= res_t>>4;
+	res_t ^= res_t>>2;
+	res_t ^= res_t>>1;
+	cpu.PF=!res_t;
 
 	print_asm_template2();
 }
