1acbbed553003ce974f5edc9b74a76d728af4b24
tracer-ics2015 <tracer@njuics.org>
2016-03-10 15:54:57 +0800

    >  compile NEMU 14307130122 djr Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  15:54:57 up 16 min,  3 users,  load average: 0.00, 0.07, 0.11 77baa4a558949de3b3db4469a2b3dc533ea2a386

--- nemu/src/cpu/exec/arith/add-template.h
+++ nemu/src/cpu/exec/arith/add-template.h
@@ -8,8 +8,8 @@ static void do_execute(){
 	DATA_TYPE flag_src=MSB(op_src->val)&1;
 	DATA_TYPE flag_res=MSB(result)&1;
 	OPERAND_W(op_dest,result);
-	unsigned int num=0;
-	int i=0;
+//	unsigned int num=0;
+//	int i=0;
 	if(result<op_src->val&&result<op_dest->val){//unchecked
 		cpu.CF=1;
 	}
@@ -24,13 +24,19 @@ static void do_execute(){
 	else{
 		cpu.OF=0;
 	}
-	for(i=0;i<8;i++){
+/*	for(i=0;i<8;i++){
 		if(result&1){
 			num++;
 		}
 		result >>= 1;
 	}
-	cpu.PF=!(num%2);
+	cpu.PF=!(num%2);*/
+	DATA_TYPE res_t=result;
+	res_t = res_t & 0xff;
+	res_t ^= res_t>>4;
+	res_t ^= res_t>>2;
+	res_t ^= res_t>>1;
+	cpu.PF=!res_t;
 	print_asm_template2();
 }
 
