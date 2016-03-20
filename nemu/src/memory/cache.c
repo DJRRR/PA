eb6ec51a07cf0d56ffcd98043e0f8135de8d3750
tracer-ics2015 <tracer@njuics.org>
2016-03-20 15:01:20 +0800

    >  compile NEMU 14307130122 djr Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  15:01:20 up  4:32,  5 users,  load average: 0.76, 0.38, 0.27 a762a9df7aa4f9b487ca53c35a3c48f3113c1871

--- nemu/src/memory/cache.c
+++ nemu/src/memory/cache.c
@@ -8,34 +8,6 @@ extern uint32_t hwaddr_read(hwaddr_t,size_t);
 extern void hwaddr_write(hwaddr_t , size_t , uint32_t);
 extern void dram_write(hwaddr_t,size_t,uint32_t);
 
-/*typedef struct {
-	unsigned int valid:1;
-	unsigned int tag:14;
-	unsigned int index:7;
-	unsigned int offset:6;
-	union {
-	   uint8_t data[64];
-	   uint32_t data_buf[16];
-	};
-}Cache_L1;
-
-Cache_L1 cache_L1[128][8];//8-way 128zu
-
-typedef struct{
-	unsigned int valid:1;
-	unsigned int dirty:1;
-	unsigned int tag:9;
-	unsigned int index:12;
-	unsigned int offset:6;
-	union {
-	   uint8_t  data[64];
-	   uint32_t data_buf[16];
-	};
-}Cache_L2;
-
-Cache_L2 cache_L2[4096][16];//16-way 4096zu*/
-
-
 void init_cache_L1(){
 	int i,j;
 	for(i=0;i<128;i++){
@@ -70,7 +42,7 @@ bool find_cache_L1(hwaddr_t addr,size_t len){
 
 bool find_cache_L2(hwaddr_t addr,size_t len){
 	unsigned int index_i=(addr>>6)&0xfff;
-	unsigned int tag_i=(addr>>(6+12))&0x1ff;
+	unsigned int tag_i=(addr>>18)&0x1ff;
 	int i=0;
 	for(;i<16;i++){
 		if(tag_i==cache_L2[index_i][i].tag){
