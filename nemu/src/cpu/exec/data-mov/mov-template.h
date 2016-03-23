dce24e2984e64802a4c53b74784506bf509140da
tracer-ics2015 <tracer@njuics.org>
2016-03-23 13:03:35 +0800

    >  compile NEMU 14307130122 djr Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  13:03:35 up  3:06,  4 users,  load average: 0.08, 0.03, 0.05 8b8837e72bce75fe8c4e764cb3b0fad1b9cbce70

--- nemu/src/cpu/exec/data-mov/mov-template.h
+++ nemu/src/cpu/exec/data-mov/mov-template.h
@@ -18,17 +18,22 @@ make_helper(concat(mov_cr2r_,SUFFIX)){
 	uint32_t judge=instr_fetch(cpu.eip+1,1);
 //	printf("0x%.2X\n",judge);
 	if(judge==0xc0){//cr0
-		OPERAND_W(op_dest,cpu.cr0.val);
+	//	REG(op_src->reg)=cpu.cr0.val;
+		print_asm("mov cr0 ,  %%%s",REG_NAME(op_src->reg));
 
 	}
 	else if(judge==0xd8){//cr3
+//		REG(op_src->reg)=cpu.cr3.val;
+		print_asm("mov cr3 , %%%s",REG_NAME(op_src->reg));
 	}
+	printf("%s\n",REG_NAME(op_src->reg));
 	return 2;
 
 }
 make_helper(concat(mov_r2cr_,SUFFIX)){
 	uint32_t judge=instr_fetch(cpu.eip+1,1);
 	if(judge==0xc0){//cr0
+	//	cpu.cr0.val=REG(op_sr
 	}
 	else if(judge==0xd8){//cr3
 	}
