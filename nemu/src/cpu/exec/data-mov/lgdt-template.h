dd92924a9d1f1a5062047be67fc70326db3ba13a
tracer-ics2015 <tracer@njuics.org>
2016-03-23 10:29:12 +0800

    >  compile NEMU 14307130122 djr Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  10:29:12 up 32 min,  4 users,  load average: 0.00, 0.01, 0.02 429bbe7ee77bad204b9908a779244b8b89142d38

--- nemu/src/cpu/exec/data-mov/lgdt-template.h
+++ nemu/src/cpu/exec/data-mov/lgdt-template.h
@@ -3,16 +3,16 @@
 #define instr lgdt
 
 static void do_execute(){
-	printf("0x%X\n",instr_fetch(cpu.eip+3,4));
+//	printf("0x%X\n",instr_fetch(cpu.eip+3,4));
 	hwaddr_t addr=instr_fetch(cpu.eip+3,4);
-	printf("0x%X\n",addr);
+//	printf("0x%X\n",addr);
 	if(ops_decoded.is_data_size_16){
-		cpu.gdtr.bound=swaddr_read(addr,2);
-		cpu.gdtr.base=swaddr_read(addr+2,3);
-
-
+		cpu.gdtr.bound=swaddr_read(addr,2)&0x0000ffff;
+		cpu.gdtr.base=swaddr_read(addr+2,3)&0x00ffffff;
 	}
 	else{
+		cpu.gdtr.bound=swaddr_read(addr,2)&0x0000ffff;
+		cpu.gdtr.base=swaddr_read(addr+2,4);
 	}
 	print_asm_template1();
 }
--- nemu/src/cpu/exec/data-mov/lidt-template.h
+++ nemu/src/cpu/exec/data-mov/lidt-template.h
@@ -3,9 +3,14 @@
 #define instr lidt
 
 static void do_execute(){
+	hwaddr_t addr=instr_fetch(cpu.eip+3,4);
 	if(ops_decoded.is_data_size_16){
+		cpu.idtr.bound=swaddr_read(addr,2)&0x0000ffff;
+		cpu.idtr.base=swaddr_read(addr+2,3)&0x00ffffff;
 	}
 	else{
+		cpu.idtr.bound=swaddr_read(addr,2)&0x0000ffff;
+		cpu.idtr.base=swaddr_read(addr+2,4);
 	}
 	print_asm_template1();
 }
