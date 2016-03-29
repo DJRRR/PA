00b438002ce1ee6f130467c69c99ce681e42d21e
tracer-ics2015 <tracer@njuics.org>
2016-03-29 22:25:11 +0800

    >  compile NEMU 14307130122 djr Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  22:25:11 up 48 min,  3 users,  load average: 0.01, 0.04, 0.05 81b18ea7c1f1ae1dde7fccf0b38cd1aff1bed2

--- nemu/src/cpu/exec/data-mov/mov-template.h
+++ nemu/src/cpu/exec/data-mov/mov-template.h
@@ -40,7 +40,7 @@ make_helper(concat(mov_r2cr_,SUFFIX)){
 		cpu.cr3.val=REG(R_EAX);
 		printf("here!\n");
 		init_page_L1();
-		print_asm("mov %%%s,cr3",REG_NAME(R_EAX));
+		print_asm("mov %%%s,cr3",REG_NAME(op_src->val));
 	}
 	return 2;
 }
