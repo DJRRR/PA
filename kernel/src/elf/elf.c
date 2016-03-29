d444ed336cb5adc09602e4c1229bc59c0c77adc2
tracer-ics2015 <tracer@njuics.org>
2016-03-29 22:07:44 +0800

    >  compile kernel 14307130122 djr Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  22:07:44 up 31 min,  3 users,  load average: 0.00, 0.01, 0.05 52c4b6cafb1b8bd10ffea9175b02f27dc9ba531

--- kernel/src/elf/elf.c
+++ kernel/src/elf/elf.c
@@ -46,19 +46,22 @@ uint32_t loader() {
 			/* TODO: read the content of the segment from the ELF file 
 			 * to the memory region [VirtAddr, VirtAddr + FileSiz)
 			 */
-			 ramdisk_read((void *)ph->p_vaddr,ELF_OFFSET_IN_DISK+ph->p_offset,ph->p_filesz);
+		//	 ramdisk_read((void *)vaddr,ELF_OFFSET_IN_DISK+ph->p_offset,ph->p_filesz);
 			 
 			/* TODO: zero the memory region 
 			 * [VirtAddr + FileSiz, VirtAddr + MemSiz)
 			 */
-			memset((void *)(ph->p_vaddr+ph->p_filesz),0,ph->p_memsz-ph->p_filesz);
+		//	memset((void *)(vaddr+ph->p_filesz),0,ph->p_memsz-ph->p_filesz);
 
 #ifdef IA32_PAGE
 			/* Record the program break for future use. */
 			extern uint32_t brk;
 			uint32_t new_brk = ph->p_vaddr + ph->p_memsz - 1;
 			if(brk < new_brk) { brk = new_brk; }
+			uint32_t vaddr=mm_malloc(ph->p_vaddr,ph->p_memsz);
 #endif
+			ramdisk_read((void *)vaddr,ELF_OFFSET_IN_DISK+ph->p_offset,ph->p_filesz);
+			memset((void *)(vaddr+ph->p_filesz),0,ph->p_memsz-ph->p_filesz);
 		}
 	}
 
