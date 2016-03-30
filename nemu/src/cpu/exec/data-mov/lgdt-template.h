7a4a0662d07e04497100e8f274d70ec860417547
tracer-ics2015 <tracer@njuics.org>
2016-03-30 11:31:42 +0800

    >  compile NEMU 14307130122 djr Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  11:31:42 up  1:31,  4 users,  load average: 0.00, 0.01, 0.05 adb09a59bafaf913de8b4c1621e1b9dd17e939c3

--- nemu/src/cpu/exec/data-mov/lgdt-template.h
+++ nemu/src/cpu/exec/data-mov/lgdt-template.h
@@ -5,6 +5,8 @@
 static void do_execute(){
 //	printf("0x%X\n",instr_fetch(cpu.eip+3,4));
 	hwaddr_t addr=instr_fetch(cpu.eip+3,4);
+	uint32_t judge=instr_fetch(cpu.eip+2,1);
+	printf("!!!!!!!!!%.2X\n",judge);
 //	printf("0x%X\n",addr);
 	if(ops_decoded.is_data_size_16){
 		cpu.gdtr.limit=swaddr_read(addr,2,S_CS)&0x0000ffff;
