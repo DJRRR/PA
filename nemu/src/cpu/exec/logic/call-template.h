7a36a61f95e659caef22b5e918f28b9f0c611f83
tracer-ics2015 <tracer@njuics.org>
2016-03-30 22:41:29 +0800

    >  compile NEMU 14307130122 djr Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  22:41:29 up  1:24,  4 users,  load average: 0.10, 0.06, 0.07 63171df1bfbf5dad987a7452d32ee5bd274e30d0

--- nemu/src/cpu/exec/logic/call-template.h
+++ nemu/src/cpu/exec/logic/call-template.h
@@ -1,40 +1,34 @@
 #include "cpu/exec/template-start.h"
+#include "cpu/decode/modrm.h"
 
 #define instr call
 
 static void do_execute(){
-	int current_sreg=S_CS;
-	if(cpu.eip<=0x8049000){
-		printf("cpu.eip:0x%x\n",cpu.eip);
-		puts("1");
-	}
-	DATA_TYPE_S addr=instr_fetch(cpu.eip+1,DATA_BYTE);
+	int current_sreg=S_SS;
 	if(ops_decoded.opcode==0xe8){
 		if(ops_decoded.is_data_size_16){
 		//	cpu.esp -= 2;
 		//	MEM_W(cpu.esp,(cpu.eip&0xff)+1+DATA_BYTE);
 		//	cpu.eip +=op_src->val&0xffff;
 			cpu.esp-=2;
-			MEM_W(cpu.esp,cpu.eip+1+DATA_BYTE);
-			cpu.eip += addr;
-			cpu.eip &= 0xffff;
+			MEM_W(cpu.esp,(cpu.eip&0xff)+1+DATA_BYTE);
+			cpu.eip += op_src->val&0xffff;
 		}
 		else{
-			if(cpu.eip==0x80480ad){
+		/*	if(cpu.eip==0x80480ad){
 				printf("here2\n");
 				printf("call : 0x%x\n",addr);
 				printf("esp : 0x%x\n",cpu.esp);
-			}
+			}*/
 			cpu.esp -= 4;
 			MEM_W(cpu.esp,cpu.eip+1+DATA_BYTE);
-			if(cpu.eip==0x80480ad){
+	/*		if(cpu.eip==0x80480ad){
 				printf("here3\n");
-			}
-			cpu.eip+=addr;
+			}*/
+			cpu.eip+=op_src->val;
 		}
 	}
 	if(ops_decoded.opcode==0xff){
-/*		printf("here\n");
 		if(ops_decoded.is_data_size_16){
 			cpu.esp -= 2;
 			MEM_W(cpu.esp,(cpu.eip&0xff)+2);
@@ -42,19 +36,17 @@ static void do_execute(){
 		}
 		else{
 			cpu.esp -= 4;
-			MEM_W(cpu.esp,cpu.eip+2);
-			cpu.eip=op_src->val-2;
-		}*/
-		uint32_t buf=2;
-		if(instr_fetch(cpu.eip+1,1)==0x57){
-			buf=3;
+			unsigned int modrm = instr_fetch(cpu.eip+1,1);
+			unsigned int mod=modrm>>6;
+			if(mod==1 || mod==2){
+				MEM_W(cpu.esp,cpu.eip+3);
+				cpu.eip = op_src->val-3;
+			}
+			else{
+				MEM_W(cpu.esp,cpu.eip+2);
+				cpu.eip=op_src->val-2;
+			}
 		}
-		MEM_W(cpu.esp,cpu.eip+buf);
-		cpu.eip = op_src->val;
-		if(DATA_BYTE==2){
-			cpu.eip &= 0x0000ffff;
-		}
-		cpu.eip -= buf;
 	}
 	print_asm_template1();
 
