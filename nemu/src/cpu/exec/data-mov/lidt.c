327fbfc7eba3c3c7a18d91d62d66485e3c696191
tracer-ics2015 <tracer@njuics.org>
2016-04-20 12:17:12 +0800

    >  compile NEMU 14307130122 djr Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  12:17:12 up  1:27,  4 users,  load average: 0.01, 0.06, 0.10 a1deaffeb9c03e6d836bded35f2cfe0e45b28014

--- nemu/src/cpu/exec/data-mov/lidt.c
+++ nemu/src/cpu/exec/data-mov/lidt.c
@@ -2,18 +2,25 @@
 #include "cpu/decode/modrm.h"
 
 make_helper(lidt_m){
-	if(instr_fetch(eip+1,1)==0x15){
-		swaddr_t addr=instr_fetch(eip+2,4);
+	if(instr_fetch(eip+1,1)==0x18){
+	/*	swaddr_t addr=instr_fetch(eip+2,4);
 		cpu.idtr.limit=swaddr_read(addr,2,S_SS);
 		cpu.idtr.base=swaddr_read(addr+2,4,S_SS);
 		print_asm("lidt");
-		return 6;
-	}
-	else{
+		return 6;*/
 		cpu.idtr.limit=swaddr_read(cpu.eax,2,S_SS);
-		cpu.idtr.base=swaddr_read(cpu.eax,2,S_SS);
+		cpu.idtr.base=swaddr_read(cpu.eax+2,2,S_SS);
 		print_asm("lidt (%%eax)");
 		return 2;
+
+	}
+	else{
+		printf("lidt error!\n");
+		assert(0);
+		cpu.idtr.limit=swaddr_read(cpu.eax,2,S_SS);
+		cpu.idtr.base=swaddr_read(cpu.eax+2,2,S_SS);
+		print_asm("lidt (%%eax)");
+		return 6;
 	}
 	return 0;
 }
