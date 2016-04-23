e33eacdd93e8db6968cc8e19dfe8d6cfdce5cefb
tracer-ics2015 <tracer@njuics.org>
2016-04-23 12:35:09 +0800

    >  compile NEMU 14307130122 djr Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  12:35:09 up 38 min,  2 users,  load average: 0.01, 0.04, 0.05 7adbd146b40f1cf6d21b8b8baa4711e81fb07581

--- nemu/src/cpu/exec/logic/call-template.h
+++ nemu/src/cpu/exec/logic/call-template.h
@@ -3,7 +3,7 @@
 
 #define instr call
 
-/*static void do_execute(){
+static void do_execute(){
 	int current_sreg=S_SS;
 	if(ops_decoded.opcode==0xe8){
 		if(ops_decoded.is_data_size_16){
@@ -30,49 +30,21 @@
 			cpu.esp -= 4;
 			unsigned int modrm = instr_fetch(cpu.eip+1,1);
 			unsigned int mod=modrm>>6;
-			int buf=0;
 			if(mod==1 || mod==2){
-				buf=3;
-				MEM_W(cpu.esp,cpu.eip+buf);
-				cpu.eip = op_src->val;
+				MEM_W(cpu.esp,cpu.eip+3);
+				cpu.eip = op_src->val-3;
 			}
 			else{
-				buf=2;
-				MEM_W(cpu.esp,cpu.eip+buf);
-				cpu.eip=op_src->val;
+				MEM_W(cpu.esp,cpu.eip+2);
+				cpu.eip=op_src->val-2;
 			}
 			if(DATA_BYTE==2){
 				cpu.eip &= 0x0000ffff;
 			}
-			cpu.eip -=buf;
 		}
 	}
 	print_asm_template1();
 
-}*/
-static void do_execute(){
-//	swaddr_t opcode=instr_fetch(cpu.eip,1);
-//	DATA_TYPE_S addr=instr_fetch(cpu.eip+1,DATA_BYTE);
-	int current_sreg=S_SS;
-	if(op_src->type==2){
-		cpu.esp -= DATA_BYTE;
-		MEM_W(cpu.esp,cpu.eip+DATA_BYTE+1);
-		cpu.eip += (int)op_src->val;
-		if(DATA_BYTE==2) cpu.eip &=0x0000ffff;
-		print_asm("call %x",cpu.eip);
-	}
-	else{
-		uint32_t buf=2;
-		if(instr_fetch(cpu.eip+1,1)==0x57) buf=3;
-		cpu.esp -=DATA_BYTE;
-		MEM_W(cpu.esp,cpu.eip+2);
-		cpu.eip=op_src->val;
-		if(DATA_BYTE==2){
-			cpu.eip &= 0x0000ffff;
-		}
-		cpu.eip -= buf;
-		print_asm_template1();
-	}
 }
 
 
