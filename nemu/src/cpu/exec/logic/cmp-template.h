551208cb7fe505b5dafccbe6948c7c19c985ba21
tracer-ics2015 <tracer@njuics.org>
2016-04-23 13:21:30 +0800

    >  compile NEMU 14307130122 djr Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  13:21:30 up  1:24,  3 users,  load average: 0.00, 0.01, 0.05 87e21605982aa394b3109a36ba5aefd3a7a57e3b

--- nemu/src/cpu/exec/logic/cmp-template.h
+++ nemu/src/cpu/exec/logic/cmp-template.h
@@ -3,12 +3,10 @@
 #define instr cmp
 
 static void do_execute(){
-	DATA_TYPE result= op_dest->val - (DATA_TYPE_S)op_src->val;
+	DATA_TYPE_S result= op_dest->val - (DATA_TYPE_S)op_src->val;
 	DATA_TYPE flag_dest=MSB(op_dest->val)&1;
 	DATA_TYPE flag_src=MSB(op_src->val)&1;
 	DATA_TYPE flag_res=MSB(result)&1;
-//	unsigned int num=0;
-//	int i=0;
 	cpu.ZF=!result;
 	cpu.SF=flag_res;
 	if((flag_dest==1&&flag_src==0&&flag_res==0)||(flag_dest==0&&flag_src==1&&flag_res==1)){
@@ -23,13 +21,6 @@ static void do_execute(){
 	else{
 		cpu.CF=0;
 	}
-/*	for(i=0;i<8;i++){
-		if(result&1){
-			++num;
-		}
-		result >>= 1;
-	}	
-	cpu.PF=!(num%2);*/
 	DATA_TYPE res_t=result;
 	res_t = res_t &0xff;
 	res_t ^= res_t>>4;
