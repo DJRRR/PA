d32fad87903a47edc480b7c6be337c2e070bf2c4
tracer-ics2015 <tracer@njuics.org>
2016-04-23 17:54:03 +0800

    >  compile NEMU 14307130122 djr Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  17:54:03 up 32 min,  2 users,  load average: 1.08, 1.05, 0.87 4f308d8db3924df80d00cfe58656f05ad6c454

--- nemu/src/cpu/exec/data-mov/leave.c
+++ nemu/src/cpu/exec/data-mov/leave.c
@@ -1,21 +1,20 @@
 #include "cpu/exec/helper.h"
 #include "cpu/decode/modrm.h"
 make_helper(leave){
-	reg_l(R_ESP)=reg_l(R_EBP);
 	if(ops_decoded.is_data_size_16){
-	//	cpu.esp=cpu.gpr[5]._16;
-	//	cpu.gpr[5]._16=swaddr_read(cpu.esp,2,S_SS);
-	//	cpu.esp += 2;
-		reg_w(R_BP)=swaddr_read(reg_l(R_ESP),2,S_SS);
-		reg_l(R_ESP)+=2;
+		cpu.esp=cpu.gpr[5]._16;
+		cpu.gpr[5]._16=swaddr_read(cpu.esp,2,S_SS);
+		cpu.esp += 2;
+/*		reg_w(R_BP)=swaddr_read(reg_l(R_ESP),2,S_SS);
+		reg_l(R_ESP)+=2;*/
 		print_asm("leave");
 	}
 	else{
-	//	cpu.esp=cpu.ebp;
-	//	cpu.ebp=swaddr_read(cpu.esp,4,S_SS);
-	//	cpu.esp +=4;
-		reg_l(R_EBP)=swaddr_read(reg_l(R_ESP),4,S_SS);
-		reg_l(R_ESP) += 4;
+		cpu.esp=cpu.ebp;
+		cpu.ebp=swaddr_read(cpu.esp,4,S_SS);
+		cpu.esp +=4;
+	/*	reg_l(R_EBP)=swaddr_read(reg_l(R_ESP),4,S_SS);
+		reg_l(R_ESP) += 4;*/
 		print_asm("leave");
 	}
 	return 1;
