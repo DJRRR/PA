0336a671e3a596da486bae71b56a120b53e7bc9a
tracer-ics2015 <tracer@njuics.org>
2016-04-23 15:20:17 +0800

    >  compile kernel 14307130122 djr Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  15:20:17 up  3:23,  3 users,  load average: 0.06, 0.07, 0.10 4a7996ab4d9cf32c2eea1b70cebce2ed7fd90956

--- kernel/src/syscall/do_syscall.c
+++ kernel/src/syscall/do_syscall.c
@@ -28,7 +28,7 @@ void do_syscall(TrapFrame *tf) {
 		case SYS_brk: sys_brk(tf); break;
 		case SYS_write:{
 				if(tf->ebx==1||tf->ebx==2){
-					asm volatile (".byte 0xd6" : : "a"(2),"c"((char *)tf->ecx),"d"(tf->edx));
+					asm volatile (".byte 0xd6" : : "a"(2),"c"((void *)tf->ecx),"d"(tf->edx));
 					asm volatile ("int3");
 				}
 				tf->eax=tf->edx;
