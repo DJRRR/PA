a0af669497a6b1939b7a49e96f8b1b551a9cf05f
tracer-ics2015 <tracer@njuics.org>
2016-04-26 18:10:01 +0800

    >  compile NEMU 14307130122 djr Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  18:10:01 up 7 min,  2 users,  load average: 0.00, 0.05, 0.05 a57e73a16d7a650d6ace6a84afbadde19ed6c7b9

--- nemu/src/cpu/exec/special/special.c
+++ nemu/src/cpu/exec/special/special.c
@@ -28,13 +28,11 @@ make_helper(nemu_trap) {
 	print_asm("nemu trap (eax = %d)", cpu.eax);
 	 int i=0;
 	switch(cpu.eax) {
-		case 2:{
+		case 2:
 			for(i=0;i<cpu.edx;i++){
 				printf("%c",swaddr_read(cpu.ecx+i,1,S_DS));
 			}
 		   	break;
-			   }
-			   break;
 		default:
 			printf("\33[1;31mnemu: HIT %s TRAP\33[0m at eip = 0x%08x\n\n",
 					(cpu.eax == 0 ? "GOOD" : "BAD"), cpu.eip);
