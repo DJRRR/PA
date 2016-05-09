a08c652889d6e860afc83ca8fb9dae5cd5b04453
tracer-ics2015 <tracer@njuics.org>
2016-05-09 14:05:38 +0800

    >  compile game 14307130122 djr Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  14:05:38 up  1:47,  3 users,  load average: 0.01, 0.04, 0.05 6262da0d37037f5041becf20f71562fb2c226f19

--- kernel/src/memory/vmem.c
+++ kernel/src/memory/vmem.c
@@ -26,12 +26,15 @@ void create_video_mapping() {
 		pframe += PAGE_SIZE;
 		ptable++;
 	}*/
-	PDE*pdir=get_updir()+((VMEM_ADDR>>22)&0X3ff);
-	PTE*ptable=vptable+((VMEM_ADDR>>12)&0x3ff);
-	pdir->val=make_pde(va_to_pa(vptable));
-	uint32_t page_no;
-	for(page_no=0;page_no<SCR_SIZE;page_no += PAGE_SIZE){
-		ptable->val=make_pte(page_no+VMEM_ADDR);
+	PDE* pdir=get_updir();
+	PTE* ptable=(PTE*)va_to_pa(vptable);
+	pdir[0].val=make_pde(ptable);
+	int i=0;
+	uint32_t pframe_addr=VMEM_ADDR;
+	ptable = ptable +0xa0;
+	for(;i<16;i++){
+		ptable->val=make_pte(pframe_addr);
+		pframe_addr += 4096;
 		ptable++;
 	}
 }
