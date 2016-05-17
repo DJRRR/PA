f986fbcfbcd3ecc8418ff6861319dc1567860eb1
tracer-ics2015 <tracer@njuics.org>
2016-05-17 12:19:41 +0800

    >  compile NEMU 14307130122 djr Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  12:19:41 up  1:35,  3 users,  load average: 0.00, 0.15, 0.16 890f70e6b6fb0337c42b0a507268e43efa0c1fa6

--- nemu/src/memory/page.c
+++ nemu/src/memory/page.c
@@ -95,13 +95,13 @@ typedef struct{
 	bool valid;
 	uint32_t tag;
 	PTE pte;
-}TLB[8];
+}TLB[64];
 
 TLB tlb;
 
 void init_TLB(){
 	int i;
-	for(i=0;i<8;i++){
+	for(i=0;i<64;i++){
 		tlb[i].valid=0;
 	}
 }
@@ -113,12 +113,12 @@ hwaddr_t read_page(lnaddr_t addr){
 	ln_addr lnaddr;
 	lnaddr.val=addr;
 	int i;
-	for(i=0;i<8;i++){
+	for(i=0;i<64;i++){
 		if(tlb[i].valid&&(tlb[i].tag==lnaddr.tag)) break;
 		if(!tlb[i].valid) break;
 	}
-	if(tlb[i].valid==0||i==8){
-		if(i==8) i=addr%8;
+	if(tlb[i].valid==0||i==64){
+		if(i==64) i=addr%64;
 		hwaddr_t pde_addr=(cpu.cr3.page_directory_base<<12)+lnaddr.dir*sizeof(PDE);//4
 		PDE pde;
 		pde.val=hwaddr_read(pde_addr,4);
