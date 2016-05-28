70ce24c76182f96f588e6d34c69bf14505000e5b
tracer-ics2015 <tracer@njuics.org>
2016-05-28 19:30:14 +0800

    >  compile game 14307130122 djr Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  19:30:14 up  4:25,  4 users,  load average: 0.00, 0.02, 0.05 dbc1e5226900189bcf378199bc29b316f265142

--- game/src/common/lib/syscall.c
+++ game/src/common/lib/syscall.c
@@ -15,8 +15,9 @@ void _exit(int status) {
 }
 
 int open(const char *pathname, int flags) {
-	nemu_assert(0);
-	return 0; 
+//	nemu_assert(0);
+	return syscall(SYS_open,pathname,flags);
+//	return 0; 
 }
 
 int read(int fd, char *buf, int len) {
