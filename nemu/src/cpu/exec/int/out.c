b5620b83db69c586b998404a2ea9bfec19c79568
tracer-ics2015 <tracer@njuics.org>
2016-05-29 16:51:55 +0800

    >  compile NEMU 14307130122 djr Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  16:51:55 up  6:30,  3 users,  load average: 0.08, 0.03, 0.07 1c8053ff9baa094f9b3e297cb6f06f7139a7cce0

--- nemu/src/cpu/exec/int/out.c
+++ nemu/src/cpu/exec/int/out.c
@@ -1,16 +1,41 @@
 #include "cpu/exec/helper.h"
 
-#define DATA_BYTE 1
-#include "out-template.h"
-#undef DATA_BYTE
+uint32_t pio_read(ioaddr_t,size_t);
+uint32_t pio_write(ioaddr_t,size_t,uint32_t);
 
-#define DATA_BYTE 2
-#include "out-template.h"
-#undef DATA_BYTE
+make_helper(out_i_b){
+	int len=decode_i_b(eip+1)+1;
+	pio_write(op_src->val,1,reg_b(R_AL));
+	print_asm("out" " al, 0x%x", op_src->val);
+	return len;
+}
+make_helper(out_i_w){
+	int len=decode_i_b(eip+1)+1;
+	pio_write(op_src->val,2,reg_w(R_AX));
+	print_asm("out" " ax, 0x%x", op_src->val);
+	return len;
+}
+make_helper(out_i_l){
+	int len=decode_i_b(eip+1)+1;
+	pio_write(op_src->val,4,reg_l(R_EAX));
+	print_asm("out" " eax, 0x%x", op_src->val);
+	return len;
+}
+make_helper(out_b){
+	pio_write(reg_w(R_DX),1,reg_b(R_AL));
+	print_asm("out" " al, (dx) ");
+	return 1;
+}
+make_helper(out_w){
+	pio_write(reg_w(R_DX),2,reg_w(R_AX));
+	print_asm("out" " ax, (dx) ");
+	return 1;
+}
+make_helper(out_l){
+	pio_write(reg_w(R_DX),4,reg_l(R_EAX));
+	print_asm("out" " eax, (dx) ");
+	return 1;
+}
 
-#define DATA_BYTE 4
-#include "out-template.h"
-#undef DATA_BYTE
-
-make_helper_v(out_i82a)
-make_helper_v(out_d2a)
+make_helper_v(out)
+make_helper_v(out_i)
