6a4bc426fc57cc32da68200d1b1f99f9b41fb855
tracer-ics2015 <tracer@njuics.org>
2016-05-30 19:28:02 +0800

    >  compile game 14307130122 djr Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  19:28:02 up 25 min,  3 users,  load average: 0.03, 0.22, 0.20 d911efbf7adf9aeb0ef99d1211c3c24fb997ff

--- game/src/nemu-pal/hal/video.c
+++ game/src/nemu-pal/hal/video.c
@@ -91,8 +91,15 @@ void SDL_UpdateRect(SDL_Surface *screen, int x, int y, int w, int h) {
 	}
 
 	/* TODO: Copy the pixels in the rectangle area to the screen. */
-
-	assert(0);
+	int i,j;
+	int cnt=screen->pitch/screen->w;
+	for(i=x*cnt;i<(x+w)*cnt;i++){
+		for(j=y;j<y+h;j++){
+			draw_pixel(i,j,screen->pixels[i*screen->h+j]);
+		}
+	}
+	
+//	assert(0);
 }
 
 void SDL_SetPalette(SDL_Surface *s, int flags, SDL_Color *colors, 
